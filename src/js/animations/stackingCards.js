804371f3009168346d0568b53ef01d4adec4d471
Evgenii Borisovich <anarkilimitz@gmail.com>
2026-03-06 18:53:55 +0500

    Добавил закоммиченные настройки для анимации второй секции, просто для эксперимента

--- src/js/animations/stackingCards.js
+++ src/js/animations/stackingCards.js
@@ -11,7 +11,7 @@ export function initStackingCards() {
 				start: 'top top',
 				// end определяет, как долго секция будет "закреплена"
 				// 150% значит: высота экрана + еще половина скролла
-				end: '+=150%',
+				end: '+=100%',
 				pin: true,
 				pinSpacing: false,
 				scrub: true,
@@ -19,16 +19,28 @@ export function initStackingCards() {
 		});
 
 		// 1. ПАУЗА (Секция стоит на месте)
-		tl.to({}, { duration: 0.2 });
+		tl.to({}, { duration: 0.3 });
 
 		// 2. АНИМАЦИЯ (Секция улетает)
+
 		tl.to(section, {
-			scale: 0.0,
-			xPercent: 150, // Гарантированно улетит за край любого экрана
-			y: -500,
+			scale: 1.0,
+			xPercent: 0, // Гарантированно улетит за край любого экрана
+			y: 0,
 			filter: 'blur(20px)',
 			duration: 1,
+			opacity: 0,
 			ease: 'none',
 		});
 	});
 }
+
+// Если надо сдвиг вправо просто то эти настройки
+// tl.to(section, {
+// 	scale: 0.0,
+// 	xPercent: 150, // Гарантированно улетит за край любого экрана
+// 	y: -500,
+// 	filter: 'blur(20px)',
+// 	duration: 1,
+// 	ease: 'none',
+// });
