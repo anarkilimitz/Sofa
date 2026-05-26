098878b92d5a9f835d18273662bcddecd4123215
Evgenii Borisovich <anarkilimitz@gmail.com>
2026-05-26 13:25:38 +0500

    Remove backend folder from tracking

--- backend/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('SMTP_HOST', 'smtp.yandex.ru');
-define('SMTP_USER', 'devlimitz@yandex.ru');
-define('SMTP_PASS', 'qzbdihuphanszngb');
-define('SMTP_PORT', 465);
-
-define('MAIL_TO', 'devlimitz@yandex.ru');
-define('MAIL_TO_NAME', 'Первый Мягкий');
